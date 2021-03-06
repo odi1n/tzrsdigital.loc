dce196cdcefe94705c35a611a7a8cfe6cd56a8a1
odi1n <sashf1999@mail.ru>
2021-03-06 03:23:37 +0300

    product.blade.php - добавил свойства

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,7 +5,65 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+                <form action="" class="w-100">
+                    <div class="form-group col">
+                        <div class="form-group">
+                            <h5>Цена</h5>
+                        </div>
+                        <hr>
+                        <div class="form-group">
+                            <div class="form-group row align-content-center">
+                                <label for="priceFrom" class=" col-2 form-label">От:</label>
+                                <div class="col-10">
+                                    <input type="number" id="priceFrom">
+                                </div>
+                            </div>
+
+                            <div class="form-group row align-content-center">
+                                <label for="priceTo" class="col-2 form-label">До:</label>
+                                <div class="col-10">
+                                    <input type="number" id="priceTo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col">
+                        <div class="form-group">
+                            <h5>Количество</h5>
+                        </div>
+                        <hr>
+                        <div class="form-group">
+                            <div class="form-group row">
+                                <div class="col-12">
+                                    <input class="w-100" type="number" id="priceFrom">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col">
+                        <div class="form-group">
+                            <h5>Название</h5>
+                        </div>
+                        <hr>
+                        <div class="form-group row">
+                            <div class="col-12">
+                                <input class="w-100" type="text" id="priceTo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col text-center">
+                        <button class="btn btn-primary w-100">Фильтр</button>
+                    </div>
+                    <div class="form-group col text-center">
+                        <button class="btn btn-dark w-100">Сброс</button>
+                    </div>
+                </form>
+            </div>
+            <!--properties-->
+
             @foreach($products as $product)
+                {{--                <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 box-shadow p-3">--}}
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 box-shadow p-3">
                     <div class="card text-center">
                         <a href="{{route('user.properties', ['category'=>$product->category_id,'product_id'=>$product->id])}}">
